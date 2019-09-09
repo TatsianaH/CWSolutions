e72dc06217675b63e20ff8162de941e4ac56772d
TatsianaH <ourflsun@gmail.com>
2019-09-09 19:38:04 -0400

    changed file

---
+++ Cat and Mouse - Harder Version.js
@@ -0,0 +1,60 @@
+*Cat and Mouse - Harder Version*
+https://www.codewars.com/kata/57ee2a1b7b45efcf700001bf/train/javascript
+
+```javascript
+function catMouse(x, j) {
+  if (!x.includes('m') || !x.includes('C') || !x.includes('D'))
+    return 'boring without all three';
+  let numC = x.indexOf('C');
+  let numM = x.indexOf('m');
+  let numD = x.indexOf('D');
+  let res = '';
+  if (numC > numM) {
+    if (numD < numC && numD > numM && numM > numC - j - 1) {
+      res = 'Protected!';
+    } else if (numM > numC - j - 1) {
+      res = 'Caught!';
+    } else {
+      res = 'Escaped!';
+    }
+  } else if (numC < numM) {
+    if (numD > numC && numD < numM && numM < numC + j + 1) {
+      res = 'Protected!';
+    }  else if (numM < numC + j + 1) {
+      res = 'Caught!';
+    } else {
+      res = 'Escaped!';
+    }
+  }
+  return res;
+}
+```
+*2 case*
+```javascript
+function catMouse(x, j) {
+  let numC = x.indexOf('C');
+  let numM = x.indexOf('m');
+  let numD = x.indexOf('D');
+  let res = '';
+  if (numC === -1 || numM === -1 || numD === -1)
+    return 'boring without all three';
+  if (numC > numM) {
+    if (numD < numC && numD > numM && numM > numC - j - 1) {
+      res = 'Protected!';
+    } else if (numM > numC - j - 1) {
+      res = 'Caught!';
+    } else {
+      res = 'Escaped!';
+    }
+  } else if (numC < numM) {
+    if (numD > numC && numD < numM && numM < numC + j + 1) {
+      res = 'Protected!';
+    }  else if (numM < numC + j + 1) {
+      res = 'Caught!';
+    } else {
+      res = 'Escaped!';
+    }
+  }
+  return res;
+}
+```
