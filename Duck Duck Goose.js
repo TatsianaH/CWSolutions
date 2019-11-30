095fa4674bd376cbb4c773e2731b05147707b7d1
TatsianaH <ourflsun@gmail.com>
2019-11-30 00:23:23 -0500

    add solution

--- Duck Duck Goose.js
+++ Duck Duck Goose.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/duck-duck-goose/train/javascript
 
 function duckDuckGoose(p, g) {
-    if(g < 10) return p[g - 1].name;
+    if(g < p.length) return p[g - 1].name;
     else if (g % p.length === 0) return p[p.length - 1].name;
     return p[g % p.length - 1].name;
 }
