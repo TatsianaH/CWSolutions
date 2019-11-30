25251bbcf0c14101b60ac4e2f53e2b7085323690
TatsianaH <ourflsun@gmail.com>
2019-11-29 23:25:08 -0500

    add solution

---
+++ A Needle in the Haystack.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/a-needle-in-the-haystack/train/javascript
+
+function findNeedle(haystack) {
+    return `found the needle at position ${haystack.indexOf('needle')}`;
+}
