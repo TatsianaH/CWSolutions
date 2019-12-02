3efa2e8b0052d1f13ea6e9076d69c031e37eeb48
TatsianaH <ourflsun@gmail.com>
2019-12-02 01:05:32 -0500

    add solution

---
+++ Filling an array (part 1).js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/filling-an-array-part-1/train/javascript
+
+const arr = (N => {
+    let array = []
+    for(let i = 0; i < N; i++){
+        array.push(i);
+    }
+    return array;
+});
+
