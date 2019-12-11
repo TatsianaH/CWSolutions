27598c76f88d103d7d89ffa497c0572c12a5aeb1
TatsianaH <ourflsun@gmail.com>
2019-12-11 00:00:08 -0500

    add solution

--- Numbers to Objects.js
+++ Numbers to Objects.js
@@ -1,1 +1,10 @@
 // https://www.codewars.com/kata/57ced2c1c6fdc22123000316/train/javascript
+
+function numObj(s){
+    const arr = [];
+    for(let i = 0; i < s.length; i++){
+        arr[i] = {};
+        arr[i][s[i]] = String.fromCharCode(s[i]);
+    }
+    return arr;
+}
