33e2ce4f1e7b7091547c4b53338f0eefad033ddc
TatsianaH <ourflsun@gmail.com>
2019-12-17 20:31:27 -0500

    add solution

---
+++ Be Concise IV - Index of an element in an array.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/be-concise-iv-index-of-an-element-in-an-array/train/javascript
+
+function find(arr, el) {
+    return arr.indexOf(el)==-1?'Not found':arr.indexOf(el);
+}
