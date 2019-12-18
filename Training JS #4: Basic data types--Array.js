39a2283a4d992401ad428e7c5bd0bf20dc671338
TatsianaH <ourflsun@gmail.com>
2019-12-17 20:14:22 -0500

    add solution

---
+++ Training JS #4: Basic data types--Array.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/training-js-number-4-basic-data-types-array/train/javascript
+
+function getLength(arr){
+    return arr.length;
+}
+function getFirst(arr){
+    return arr[0];
+}
+function getLast(arr){
+    return arr[arr.length - 1];
+}
+function pushElement(arr){
+    var el=1;
+    arr.push(el);
+    return arr;
+}
+function popElement(arr){
+    arr.pop();
+    return arr;
+}
