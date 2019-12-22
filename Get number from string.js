d24f945c76c48ed1277b124b7716de3600782bf7
TatsianaH <ourflsun@gmail.com>
2019-12-22 16:26:57 -0500

    add solution

--- Get number from string.js
+++ Get number from string.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/get-number-from-string/train/javascript
 
 function getNumberFromString(s) {
-    return +s.replace(/[^0-9]/gi, '');
+    return +s.replace(/[^0-9]/g, '');
 }
