5b76c5102262e026290a63badefefa36c6e1a0eb
TatsianaH <ourflsun@gmail.com>
2020-01-11 14:31:48 -0500

    add solution

---
+++ Grasshopper - Make change.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/560dab9f8b50f89fd6000070/train/javascript
+
+const money = 10;
+const candy = 1.42;
+const chips = 2.40;
+const soda = 1;
+let change = money - (candy + chips + soda);
