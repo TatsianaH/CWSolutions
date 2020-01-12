fecb30fefb27d5a6e3ee065cbd807ae3a19a98f5
TatsianaH <ourflsun@gmail.com>
2020-01-11 20:24:37 -0500

    add solution

---
+++ Expressions Matter.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5ae62fcf252e66d44d00008e/train/javascript
+
+function expressionMatter(a, b, c) {
+    let arr = [];
+    arr[0] = a * (b + c);
+    arr[1] = a * b * c;
+    arr[2] = a + b * c;
+    arr[3] = (a + b) * c;
+    arr[4] = a * b + c;
+    arr[5] = a + b + c;
+    return Math.max(...arr);
+}
