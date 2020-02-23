20431bc12cd3a927d80955e9f2d8331febf0655b
TatsianaH <ourflsun@gmail.com>
2020-02-23 18:11:06 -0500

    add solution

---
+++ For Twins: 1. Types.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/for-twins-1-types/train/javascript
+
+function typeValidation(variable, type) {
+  if(typeof variable === type){
+    return true;
+  } else {
+    return false;
+  }
+}
