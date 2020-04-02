7b24b380f643dccdadf01aad210a9494a9c41d31
TatsianaH <ourflsun@gmail.com>
2020-04-02 19:49:00 -0400

    add solution

--- Polish alphabet.js
+++ Polish alphabet.js
@@ -1,1 +1,21 @@
-//
+//https://www.codewars.com/kata/57ab2d6072292dbf7c000039/train/javascript
+
+function correctPolishLetters (str) {
+    const obj = {
+        ą: 'a',
+        ć: 'c',
+        ę: 'e',
+        ł: 'l',
+        ń: 'n',
+        ó: 'o',
+        ś: 's',
+        ź: 'z',
+        ż: 'z',
+    }
+    let res = '';
+    for(let i = 0; i < str.length; i++){
+        if(obj[str[i]]) res += obj[str[i]];
+        else res += str[i];
+    }
+    return res;
+}
