4ef0dd2a33be45be12780f16b8029e6b48807a62
TatsianaH <ourflsun@gmail.com>
2020-04-05 22:22:30 -0400

    add solution

--- Selective fear of numbers.js
+++ Selective fear of numbers.js
@@ -11,3 +11,20 @@ const AmIAfraid = function(d, n){
     if(d === 'Sunday' && n === -666) return true;
     else return false;
 }
+
+
+// 2 case
+
+function AmIAfraid(day, num) {
+    switch (day) {
+        case "Monday":    return num == 12
+        case "Tuesday":   return num > 95
+        case "Wednesday": return num == 34
+        case "Thursday":  return num == 0
+        case "Friday":    return num % 2 == 0
+        case "Saturday":  return num == 56
+        case "Sunday":    return num == 666 || num == -666
+    }
+
+    return false
+}
