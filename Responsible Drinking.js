dd4cf54f317d14402cf705585087bcc72bbf3084
TatsianaH <ourflsun@gmail.com>
2020-04-12 20:22:30 -0400

    add solution

--- Responsible Drinking.js
+++ Responsible Drinking.js
@@ -5,3 +5,16 @@ function hydrate(s) {
     return arr === 1 ? arr + ' glass of water' : arr + ' glasses of water';
 }
 
+
+// 2 case
+
+function hydrate(s) {
+    let answer = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (parseInt(s[i]) > 0) {
+            answer += +s[i];
+        }
+    }
+    return answer > 1 ? `${answer} glasses of water`: '1 glass of water'
+}
+
