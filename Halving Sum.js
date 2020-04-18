4bcecdcbfe9d5ec944a22556edaf1533fc142398
TatsianaH <ourflsun@gmail.com>
2020-04-17 23:29:34 -0400

    add solution

--- Halving Sum.js
+++ Halving Sum.js
@@ -9,4 +9,13 @@ function halvingSum(n) {
     }
     return sum;
 }
+// 2 case
 
+function halvingSum(n) {
+    let sum = 0;
+    while(n > 0) {
+        sum += n;
+        n = Math.floor(n / 2);
+    }
+    return sum;
+}
