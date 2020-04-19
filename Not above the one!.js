8ddbdc71c26bcda06f4f7521ca77a7a83b9efb5b
TatsianaH <ourflsun@gmail.com>
2020-04-19 19:20:43 -0400

    add solution

--- Not above the one!.js
+++ Not above the one!.js
@@ -1,10 +1,11 @@
 // https://www.codewars.com/kata/5b5097324a317afc740000fe/train/javascript
 
 function binaryCleaner(arr) {
-    const arr1 = arr.filter(el => el <= 1);
+    const arr1 = [];
     const arr2 = [];
     for(let i = 0; i < arr.length; i++){
         if(arr[i] > 1) arr2.push(i);
+        else arr1.push(arr[i]);
     }
     return [arr1, arr2];
 }
