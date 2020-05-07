3b1b3f93191030b8daae6618894149e53abd963f
TatsianaH <ourflsun@gmail.com>
2020-05-07 15:28:32 -0400

    add solution

--- Adding Big Numbers.js
+++ Adding Big Numbers.js
@@ -1,1 +1,23 @@
 // https://www.codewars.com/kata/adding-big-numbers/train/javascript
+
+function add(a, b) {
+    return (Number(a) >= Number.MAX_SAFE_INTEGER || Number(b)>= Number.MAX_SAFE_INTEGER) ?
+        bigInt(a, b) : (a * 1 + b * 1) + '';
+    function bigInt(a, b){
+        let res = '';
+        let num = 0;
+        let beginnig = new Array((Math.abs(a.length - b.length)+ 1)).join('0');
+        a.length >= b.length ? b = beginnig + b : a = beginnig + a;
+        for(let i = a.length - 1; i >= 0; i--){
+            let sum = a[i] * 1 + b[i] * 1 + num;
+            if(sum >= 10){
+                sum = sum - 10;
+                num = 1;
+            } else {
+                num = 0;
+            }
+            res = sum.toString() + res;
+        }
+        return num > 0 ? num + res : res;
+    }
+}
--- README.md
+++ README.md
@@ -6,27 +6,7 @@
 *Adding Big Numbers*
 
 ```javascript
-function add(a, b) {
-  return (Number(a) >= Number.MAX_SAFE_INTEGER || Number(b)>= Number.MAX_SAFE_INTEGER) ? 
-    bigInt(a, b) : (a * 1 + b * 1) + '';
-  function bigInt(a, b){
-  let res = '';
-  let num = 0;
-  let beginnig = new Array((Math.abs(a.length - b.length)+ 1)).join('0');
-    a.length >= b.length ? b = beginnig + b : a = beginnig + a;
-  for(let i = a.length - 1; i >= 0; i--){
-    let sum = a[i] * 1 + b[i] * 1 + num;
-    if(sum >= 10){
-      sum = sum - 10;
-      num = 1;
-    } else {
-      num = 0;
-    }
-      res = sum.toString() + res;
-    }
-    return num > 0 ? num + res : res;
-  }
-}
+
 ```
 *Sort Out The Men From Boys*
 https://www.codewars.com/kata/sort-out-the-men-from-boys-1/train/javascript
