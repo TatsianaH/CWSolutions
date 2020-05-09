d4064f3b37fbace469a657b9a82625e67a60f06a
TatsianaH <ourflsun@gmail.com>
2020-05-08 22:17:54 -0400

    add solution

---
+++ Series of integers from m to n.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5841f680c5c9b092950001ae/train/javascript
+
+function generateIntegers(m, n) {
+    let res = [];
+    for(let i = m; i <= n; i++){
+        res.push(i);
+    }
+    return res;
+}
