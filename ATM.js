4e3b76a34fc46c8c6c6498bd41a3e38b985a420c
TatsianaH <ourflsun@gmail.com>
2020-05-08 20:11:58 -0400

    add solution

--- ATM.js
+++ ATM.js
@@ -15,3 +15,17 @@ function solve(n) {
         return -1;
     }
 }
+
+// 2 case
+
+function solve(n) {
+    if(n % 10 !== 0) return -1;
+    let count = 0;
+    while(n >= 500) { n -= 500; count += 1}
+    while(n >= 200) { n -= 200; count += 1}
+    while(n >= 100) { n -= 100; count += 1}
+    while(n >= 50) { n -= 50; count += 1}
+    while(n >= 20) { n -= 20; count += 1}
+    while(n >= 10) { n -= 10; count += 1}
+    return count;
+}
