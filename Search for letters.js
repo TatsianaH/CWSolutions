0054009891b888c158794cdfc14e955ca2c8c0ae
TatsianaH <ourflsun@gmail.com>
2020-05-17 14:18:26 -0400

    add solution

--- Search for letters.js
+++ Search for letters.js
@@ -1,1 +1,11 @@
 // https://www.codewars.com/kata/52dbae61ca039685460001ae/train/javascript
+
+function change(str){
+    const alph = '0abcdefghijklmnopqrstuvwxyz';
+    let res = '';
+    for(let i = 1; i <= 26; i++){
+        if(str.toLowerCase().includes(alph[i])) res += '1';
+        else res += '0';
+    }
+    return res;
+}
