10540cc8b75595714500c1dfaffb7afa629f54fd
TatsianaH <ourflsun@gmail.com>
2020-05-18 21:30:34 -0400

    add solution

--- README.md
+++ README.md
@@ -4,11 +4,7 @@
 **
 
 ```javascript
-const sequenceSum = (begin, end, step) => {
-  if((begin < end && step <= 0) || (begin > end && step >= 0)) return 0;
-    let n = Math.floor((end - begin + step)/step);// amount of numbers in progression
-  return n * (2 * begin + (n - 1) * step) / 2;// formula of arithmetic progression
-};
+
 ```
 
 *Sum of a Beach*
--- Sum of a Sequence [Hard-Core Version].js
+++ Sum of a Sequence [Hard-Core Version].js
@@ -1,1 +1,7 @@
 // https://www.codewars.com/kata/sum-of-a-sequence-hard-core-version/train/javascript
+
+const sequenceSum = (begin, end, step) => {
+    if((begin < end && step <= 0) || (begin > end && step >= 0)) return 0;
+    let n = Math.floor((end - begin + step)/step);// amount of numbers in progression
+    return n * (2 * begin + (n - 1) * step) / 2;// formula of arithmetic progression
+};
