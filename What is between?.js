a34dd3df5454224d19f80f1da3916c996aa0bca5
TatsianaH <ourflsun@gmail.com>
2020-05-24 17:00:32 -0400

    add solution

--- What is between?.js
+++ What is between?.js
@@ -1,1 +1,10 @@
 // https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/javascript
+
+function between(a, b) {
+    let res = [];
+    while(a <= b){
+        res.push(a);
+        a++;
+    }
+    return res;
+}
