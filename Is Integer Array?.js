cdbf1f42274e25bafc4bbe597235f9ef4beae8d8
TatsianaH <ourflsun@gmail.com>
2020-06-01 19:20:40 -0400

    add solution

--- Is Integer Array?.js
+++ Is Integer Array?.js
@@ -1,1 +1,6 @@
 // https://www.codewars.com/kata/52a112d9488f506ae7000b95/train/javascript
+
+function isIntArray(arr) {
+    if(!arr) return false;
+    return arr.every(el => !Number.isNaN(el) && Number.isInteger(el));
+}
