de3b347108713373604080d61af6e39eba643c14
TatsianaH <ourflsun@gmail.com>
2020-06-01 19:32:41 -0400

    add solution

--- Twin Prime.js
+++ Twin Prime.js
@@ -1,2 +1,17 @@
 // https://www.codewars.com/kata/59b7ae14bf10a402d40000f3/train/javascript
 
+function isPrime(n){
+    let count = 0;
+    if(n < 2) return false;
+    for(let i = 2; i <= Math.sqrt(n); i++){
+        if(n % i === 0) count++;
+    }
+    return count === 0;
+}
+
+function isTwinPrime(n){
+    if(isPrime(n) === false) return false;
+    let num1 = isPrime(n - 2);
+    let num2 = isPrime(n + 2);
+    return num1 === true || num2 === true ? true : false;
+}
