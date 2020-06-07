466d82e7fa3c3268f77cf914a823b9d19e90388b
TatsianaH <ourflsun@gmail.com>
2020-06-07 19:14:05 -0400

    add solution

--- Single character palindromes.js
+++ Single character palindromes.js
@@ -1,1 +1,19 @@
 // https://www.codewars.com/kata/5a2c22271f7f709eaa0005d3/train/javascript
+
+function isPalindrome(str){
+    for(let i = 0; i < str.length / 2; i++){
+        if(str[i] !== str[str.length - 1 - i]) return false;
+    }
+    return true;
+}
+
+function solve(s){
+    if(isPalindrome(s)) return 'OK';
+    let arr = s.split('');
+    for(let i = 0; i < arr.length; i++){
+        let a = [...arr];
+        a.splice(i, 1);
+        if(isPalindrome(a.join(''))) return 'remove one';
+    }
+    return 'not possible';
+}
