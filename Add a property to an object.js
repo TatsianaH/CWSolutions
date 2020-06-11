cfe232174989e7de1343984d6d8a4e8ab8ada033
TatsianaH <ourflsun@gmail.com>
2020-06-10 22:44:42 -0400

    add solution

--- Add a property to an object.js
+++ Add a property to an object.js
@@ -1,1 +1,9 @@
 // https://www.codewars.com/kata/55f2c3dde50947271200006a/train/javascript
+
+function addProperty(obj, prop, value) {
+    if(obj[prop] !== undefined) {
+        throw 'Error';
+    }
+    obj[prop] = value;
+    return obj;
+}
