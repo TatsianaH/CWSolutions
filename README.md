aeeedd5347f3a261afc7bed1e83ca11b3ab1224b
TatsianaH <ourflsun@gmail.com>
2020-06-13 22:29:54 -0400

    add solution

--- README.md
+++ README.md
@@ -4,23 +4,6 @@
 **
 
 ```javascript
-function sumOfABeach(beach) {
-  let count = 0; 
-    beach = beach.toLowerCase();
-  if(beach.includes('sand')){
-    count += beach.split('sand').length - 1;
-  }
-  if(beach.includes('water')){
-    count += beach.split('water').length - 1;
-  } 
-  if(beach.includes('fish')){
-    count += beach.split('fish').length - 1;
-  }  
-  if(beach.includes('sun')){
-    count += beach.split('sun').length - 1;
-  }
-  return count;
-}
 ```
 *Kebabize*
 https://www.codewars.com/kata/kebabize/train/javascript
--- Sum of a Beach.js
+++ Sum of a Beach.js
@@ -1,1 +1,19 @@
 // https://www.codewars.com/kata/sum-of-a-beach/train/javascript
+
+function sumOfABeach(beach) {
+    let count = 0;
+    beach = beach.toLowerCase();
+    if(beach.includes('sand')){
+        count += beach.split('sand').length - 1;
+    }
+    if(beach.includes('water')){
+        count += beach.split('water').length - 1;
+    }
+    if(beach.includes('fish')){
+        count += beach.split('fish').length - 1;
+    }
+    if(beach.includes('sun')){
+        count += beach.split('sun').length - 1;
+    }
+    return count;
+}
