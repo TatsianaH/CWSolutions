c82f1d185e8de7756702148f2904236bfdd96f53
TatsianaH <ourflsun@gmail.com>
2020-06-16 07:45:36 -0400

    add solution

--- Thinking & Testing : How many "word"?.js
+++ Thinking & Testing : How many "word"?.js
@@ -1,1 +1,18 @@
-//
+// https://www.codewars.com/kata/56eff1e64794404a720002d2/train/javascript
+
+function testit(s){
+    let count = 0;
+    let w = '';
+    s = s.toLowerCase();
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === 'w' && w === '') w += s[i];
+        if(s[i] === 'o' && w.length === 1) w += s[i];
+        if (s[i] === 'r' && w.length === 2) w += s[i];
+        if (s[i] === 'd' && w.length === 3) w += s[i];
+        if(w === 'word'){
+            count++;
+            w = '';
+        }
+    }
+    return count;
+}
