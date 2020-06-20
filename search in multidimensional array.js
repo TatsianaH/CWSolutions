9aed67ac6896f406fba69e73469a741fd4d705ad
TatsianaH <ourflsun@gmail.com>
2020-06-20 19:35:01 -0400

    add solution

--- search in multidimensional array.js
+++ search in multidimensional array.js
@@ -1,1 +1,11 @@
-//
+// https://www.codewars.com/kata/52840d2b27e9c932ff0016ae/train/javascript
+
+const locate = function(arr,value){
+
+    function flattenDeep(arr) {
+        return arr.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+    }
+
+    const arr1 = flattenDeep(arr);
+    return arr1.includes(value);
+}
