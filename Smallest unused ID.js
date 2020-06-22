a36423e3cfd0f83cd679ea6b4d5ba0a5be6192d7
TatsianaH <ourflsun@gmail.com>
2020-06-21 21:08:28 -0400

    add solution

--- Smallest unused ID.js
+++ Smallest unused ID.js
@@ -1,1 +1,10 @@
 // https://www.codewars.com/kata/55eea63119278d571d00006a/train/javascript
+
+function nextId(ids){
+    ids.sort((a, b) => a - b);
+    let max = Math.max(...ids);
+    for(let i = 0; i <= max; i++){
+        if(!ids.includes(i)) return i;
+    }
+    return max + 1;
+}
