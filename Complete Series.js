129ee942db075906d6e9906434df1979e414411b
TatsianaH <ourflsun@gmail.com>
2020-06-21 20:49:58 -0400

    add solution

--- Complete Series.js
+++ Complete Series.js
@@ -1,1 +1,11 @@
 // https://www.codewars.com/kata/580a4001d6df740d61000301/train/javascript
+
+function completeSeries(arr) {
+    let res = [];
+    let max = Math.max(...arr);
+    for(let i = 0; i <= max; i++){
+        if(arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i])) return [0];
+        res.push(i);
+    }
+    return res;
+}
