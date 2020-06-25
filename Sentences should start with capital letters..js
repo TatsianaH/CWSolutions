76a28592ef261d04bb652986848ed1619abf0185
TatsianaH <ourflsun@gmail.com>
2020-06-25 18:44:26 -0400

    add solution

--- Sentences should start with capital letters..js
+++ Sentences should start with capital letters..js
@@ -1,1 +1,10 @@
 // https://www.codewars.com/kata/5bf774a81505a7413400006a/train/javascript
+
+function fix(p){
+    if(p.length < 1) return '';
+    let arr = p.split('. ');
+    for(let i = 0; i < arr.length; i++){
+        arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+    }
+    return arr.join('. ');
+}
