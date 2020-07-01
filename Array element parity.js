199c3cdcae5c45a20e693019ba7ee708a012bb08
TatsianaH <ourflsun@gmail.com>
2020-07-01 19:23:14 -0400

    add solution

--- Array element parity.js
+++ Array element parity.js
@@ -1,2 +1,9 @@
 // https://www.codewars.com/kata/5a092d9e46d843b9db000064/train/javascript
 
+function solve(arr){
+    for(let i = 0; i < arr.length; i++){
+        if(arr.includes(arr[i]) && !arr.includes(-1 * arr[i])){
+            return arr[i];
+        }
+    }
+};
