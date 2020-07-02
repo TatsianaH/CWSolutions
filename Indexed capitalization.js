bec4501f97305e6c430f2fc5c5d11b6d25bf3ee1
TatsianaH <ourflsun@gmail.com>
2020-07-01 23:53:08 -0400

    add solution

--- Indexed capitalization.js
+++ Indexed capitalization.js
@@ -1,1 +1,10 @@
 // https://www.codewars.com/kata/59cfc09a86a6fdf6df0000f1/train/javascript
+
+function capitalize(s,arr){
+    let str = '';
+    for(let i = 0; i < s.length; i++){
+        if(arr.includes(i)) str += s[i].toUpperCase();
+        else str += s[i];
+    }
+    return str;
+};
