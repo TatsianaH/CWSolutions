8f964bcf54540dfc233ec1897b0b21b935da2e32
TatsianaH <ourflsun@gmail.com>
2020-07-03 18:17:07 -0400

    add solution

--- C.Wars.js
+++ C.Wars.js
@@ -1,1 +1,11 @@
 // https://www.codewars.com/kata/55968ab32cf633c3f8000008/train/javascript
+
+function initials(n){
+    n = n.split(' ');
+    let res = '';
+    for(let i = 0; i < n.length; i++){
+        if(i !== n.length - 1) res += n[i][0].toUpperCase() + '.';
+        else res += n[i][0].toUpperCase() + n[i].slice(1);
+    }
+    return res;
+}
