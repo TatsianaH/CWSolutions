b997629ec872aaf849a1e6ff95c925cb9ed12fd0
TatsianaH <ourflsun@gmail.com>
2020-07-07 22:21:38 -0400

    add solution

--- Training JS #11: loop statement --break,continue.js
+++ Training JS #11: loop statement --break,continue.js
@@ -1,2 +1,11 @@
 // https://www.codewars.com/kata/5721c189cdd71194c1000b9b/train/javascript
 
+function grabDoll(dolls){
+    let bag = [];
+    for(let i = 0; i < dolls.length; i++){
+        if(dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') bag.push(dolls[i]);
+        else continue;
+        if(bag.length === 3) break;
+    }
+    return bag;
+}
