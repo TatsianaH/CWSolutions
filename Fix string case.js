54332ca8cc144c885c8aa87289f2e722b01869c0
TatsianaH <ourflsun@gmail.com>
2020-07-23 22:10:57 -0400

    add solution

--- Fix string case.js
+++ Fix string case.js
@@ -1,1 +1,11 @@
 // https://www.codewars.com/kata/5b180e9fedaa564a7000009a/train/javascript
+
+function solve(s){
+    let lowerCase = 0;
+    let upperCase = 0;
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === s[i].toLowerCase())lowerCase++;
+        else upperCase++;
+    }
+    return lowerCase >= upperCase ? s.toLowerCase() : s.toUpperCase();
+}
