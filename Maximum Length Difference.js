1283cbb58aa995659531eccffd270459970d7c9c
TatsianaH <ourflsun@gmail.com>
2020-07-31 22:57:59 -0400

    add solution

--- Maximum Length Difference.js
+++ Maximum Length Difference.js
@@ -1,2 +1,12 @@
 // https://www.codewars.com/kata/5663f5305102699bad000056/train/javascript
 
+function mxdiflg(a1, a2) {
+    if(a1.length === 0 || a2.length === 0) return -1;
+    a1 = a1.map(el => el.length);
+    let min1 = Math.min(...a1);
+    let max1 = Math.max(...a1);
+    a2 = a2.map(el => el.length);
+    let min2 = Math.min(...a2);
+    let max2 = Math.max(...a2)
+    return Math.abs(min1 - max2) > Math.abs(max1 - min2) ? Math.abs(min1 - max2) : Math.abs(max1 - min2);
+}
