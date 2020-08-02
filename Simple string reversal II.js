760bc540077a61ea69bc8abc2c6af9c8edde43f4
TatsianaH <ourflsun@gmail.com>
2020-08-02 11:42:40 -0400

    add solution

--- Simple string reversal II.js
+++ Simple string reversal II.js
@@ -1,1 +1,6 @@
 // https://www.codewars.com/kata/5a8d1c82373c2e099d0000ac/train/javascript
+
+function solve(st,a,b){
+    const str = st.slice(a, b + 1).split('').reverse().join('');
+    return st.slice(0, a) + str + st.slice(b + 1);
+}
