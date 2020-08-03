42a6a65c0af7411890fc7f30dce384b690bd74d9
TatsianaH <ourflsun@gmail.com>
2020-08-03 17:08:04 -0400

    add solution

--- Simple string characters.js
+++ Simple string characters.js
@@ -1,2 +1,13 @@
 // https://www.codewars.com/kata/5a29a0898f27f2d9c9000058/train/javascript
 
+function solve(s){
+    let res = [0, 0, 0, 0];
+    const alph = 'abcdefghijklmnopqrstuvwxyz';
+    for(let i = 0; i < s.length; i++){
+        if(alph.toUpperCase().includes(s[i])) res[0]++;
+        else if(alph.includes(s[i])) res[1]++;
+        else if('0123456789'.includes(s[i])) res[2]++;
+        else res[3]++;
+    }
+    return res;
+}
