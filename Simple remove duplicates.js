91de1f05b6304d420433f31d3b4ae08962919f6e
TatsianaH <ourflsun@gmail.com>
2020-08-15 16:10:57 -0400

    add solution

--- Simple remove duplicates.js
+++ Simple remove duplicates.js
@@ -1,1 +1,9 @@
 // https://www.codewars.com/kata/5ba38ba180824a86850000f7/train/javascript
+
+function solve(arr){
+    let res = [];
+    arr.forEach((el, i) => {
+        if(arr.lastIndexOf(el) === i) res.push(el);
+    });
+    return res;
+}
