9b72e8910aa6fcef88338d7bead531e621e32c41
TatsianaH <ourflsun@gmail.com>
2020-08-15 16:14:17 -0400

    add solution

--- Sort Numbers.js
+++ Sort Numbers.js
@@ -1,1 +1,6 @@
 // https://www.codewars.com/kata/5174a4c0f2769dd8b1000003/train/javascript
+
+function solution(nums){
+    if(!nums || nums.length === 0) return [];
+    else return nums.sort((a, b) => a - b);
+}
