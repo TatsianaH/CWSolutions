46c4488cedcb9a0bfba431784184b8b67c149ae0
TatsianaH <ourflsun@gmail.com>
2020-08-14 20:02:52 -0400

    add solution

--- Alternate case.js
+++ Alternate case.js
@@ -1,2 +1,10 @@
 // https://www.codewars.com/kata/57a62154cf1fa5b25200031e/train/javascript
 
+function alternateCase(s) {
+    let word = '';
+    for(let i = 0 ; i < s.length; i++){
+        if(s[i] === s[i].toLowerCase()) word += s[i].toUpperCase();
+        else word += s[i].toLowerCase();
+    }
+    return word;
+}
