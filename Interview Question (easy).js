3094cea6834d4bdd3184e6d95b5299d2b4bbeb6f
TatsianaH <ourflsun@gmail.com>
2020-08-16 19:38:32 -0400

    add solution

--- Interview Question (easy).js
+++ Interview Question (easy).js
@@ -4,13 +4,13 @@ function getStrings(city){
     city = city.toLowerCase().replace(/[^a-z]/g, '');
     const obj = {};
     for(let i = 0; i < city.length; i++){
-        if(!obj[city[i]]) obj[city[i]] = 1;
-        else obj[city[i]]++;
+        if(!obj[city[i]]) obj[city[i]] = '*';
+        else obj[city[i]] += '*';
     }
     const arr = Object.keys(obj);
     let res = '';
     for(let i = 0; i < arr.length; i++){
-        res += arr[i] + ':' + '*'.repeat(obj[arr[i]]) + ','
+        res += arr[i] + ':' + obj[arr[i]] + ','
     }
     return res.slice(0, -1);
 }
