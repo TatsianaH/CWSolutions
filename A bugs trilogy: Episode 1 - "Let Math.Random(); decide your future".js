b6aebdf730988f8c94956396b79d10dafc9d2044
TatsianaH <ourflsun@gmail.com>
2020-08-17 17:34:44 -0400

    add solution

--- A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future".js
+++ A bugs trilogy: Episode 1 - "Let Math.Random(); decide your future".js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/562e98755e9214cd2500003d/train/javascript
 
 function yourFutureCareer(career){
-    career = Math.random() * 1;
+    career = Math.random();
     if (career <= 0.32) {
         return 'FrontEnd Developer';
     } else if (career <= 0.65){
