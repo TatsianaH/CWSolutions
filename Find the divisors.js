2633d033f2a9f305b024ec9b0b68b672ee774b9c
TatsianaH <ourflsun@gmail.com>
2020-08-17 19:29:53 -0400

    add solution

--- Find the divisors.js
+++ Find the divisors.js
@@ -9,3 +9,13 @@ const list = [];
   }
   return (list[0] !== undefined) ? list : integer + ' is prime';;
 };
+
+// 2 case
+
+function divisors(integer) {
+  const divisors = [];
+  for(let i = 2; i <= integer / 2; i++){
+    if(integer % i === 0) divisors.push(i);
+  }
+  return divisors.length > 0 ? divisors : `${integer} is prime`;
+};
