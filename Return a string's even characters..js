b43c6c31d1cdf6f31a0b35aebf451874a7fab953
TatsianaH <ourflsun@gmail.com>
2020-08-23 18:55:24 -0400

    add solution

--- Return a string's even characters..js
+++ Return a string's even characters..js
@@ -1,1 +1,6 @@
-// 
+// https://www.codewars.com/kata/566044325f8fddc1c000002c/train/javascript
+
+function evenChars(str) {
+    if(str.length < 2 || str.length > 100) return 'invalid string';
+    return str.split('').filter((el, i) => i % 2 === 1);
+}
