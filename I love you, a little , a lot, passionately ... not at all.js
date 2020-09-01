7037671b8df213a7fbfe1610e8848bb3321e63b8
TatsianaH <ourflsun@gmail.com>
2020-09-01 16:37:15 -0400

    add another solution

--- I love you, a little , a lot, passionately ... not at all.js
+++ I love you, a little , a lot, passionately ... not at all.js
@@ -4,3 +4,10 @@ function howMuchILoveYou(n) {
     const arr = ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all'];
     return n % 6 ? arr[(n % 6) - 1] : arr[5];
 }
+
+// 2 case
+
+function howMuchILoveYou(petals) {
+    const phrases = ['not at all','I love you', 'a little', 'a lot', 'passionately', 'madly'];
+    return phrases[petals % 6];
+}
