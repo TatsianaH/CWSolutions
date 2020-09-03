9fedf5e521ce74551282434c49ff5f875d30e2dd
TatsianaH <ourflsun@gmail.com>
2020-09-03 18:59:40 -0400

    add solution

--- Bases Everywhere.js
+++ Bases Everywhere.js
@@ -1,1 +1,9 @@
 // https://www.codewars.com/kata/5f47e79e18330d001a195b55/train/javascript
+
+function baseFinder(seq){
+    seq = seq.join('');
+    for(let i = 0; i < seq.length; i++){
+        if(!seq.includes(i)) return i;
+    }
+    return 10;
+}
