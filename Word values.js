c2960ba4a5f4c2856b0a25189a95fe993a31568f
TatsianaH <ourflsun@gmail.com>
2020-09-06 19:28:08 -0400

    add solution

--- Word values.js
+++ Word values.js
@@ -1,1 +1,11 @@
 // https://www.codewars.com/kata/598d91785d4ce3ec4f000018/train/javascript
+
+function wordValue(a) {
+    const alph = ' abcdefghijklmnopqrstuvwxyz';
+    a = a.map(word => word
+        .split('')
+        .map(letter => alph.indexOf(letter))
+        .reduce((acc, curr) => acc + curr, 0))
+        .map((num, i) => num * (i + 1));
+    return a;
+}
