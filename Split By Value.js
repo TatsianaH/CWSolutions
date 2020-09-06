209315a065e7825064bbacc4621a818a61177d6e
TatsianaH <ourflsun@gmail.com>
2020-09-05 21:08:58 -0400

    add solution

--- Split By Value.js
+++ Split By Value.js
@@ -1,2 +1,5 @@
 // https://www.codewars.com/kata/5a433c7a8f27f23bb00000dc/train/javascript
 
+function splitByValue(k, elem){
+    return elem.filter(el => el < k).concat(elem.filter(el => el >= k));
+}
