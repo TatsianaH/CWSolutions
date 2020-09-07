73b72368b5d353f11dbd374c85f648997ab29d85
TatsianaH <ourflsun@gmail.com>
2020-09-07 18:03:34 -0400

    add solution

--- Driving School Series #2.js
+++ Driving School Series #2.js
@@ -1,1 +1,13 @@
 // https://www.codewars.com/kata/589b1c15081bcbfe6700017a/train/javascript
+
+function cost (mins) {
+    let res = 30;
+    if(mins > 60) {
+        let i = mins - 60;
+        while(i > 5){
+            i -= 30;
+            res += 10;
+        }
+    }
+    return res;
+}
