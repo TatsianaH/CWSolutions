aaef0f36305060d0279c6d89dd7ce8e0a83c3bc4
TatsianaH <ourflsun@gmail.com>
2020-09-07 18:26:51 -0400

    add solution

--- The Skiponacci Sequence.js
+++ The Skiponacci Sequence.js
@@ -1,1 +1,11 @@
 // https://www.codewars.com/kata/580777ee2e14accd9f000165/train/javascript
+
+function skiponacci(n) {
+    let fib = [0, 1];
+    if(n === 1) return '1';
+    for(let i = 2; i <= n; i++){
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
+    const res = fib.slice(1).map((el, i) => i % 2 ? el = 'skip' : el).join(' ');
+    return res;
+}
