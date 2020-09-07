1773f694733a34bfa57949b339795c9cb9ca0c3e
TatsianaH <ourflsun@gmail.com>
2020-09-07 17:33:24 -0400

    add solution

--- Simple string division.js
+++ Simple string division.js
@@ -1,1 +1,9 @@
 // https://www.codewars.com/kata/5b83c1c44a6acac33400009a/train/javascript
+
+function solve(str,k){
+    let arr = [];
+    for(let i = 0; i < str.length; i++){
+        arr.push(str.slice(i, i + (str.length - k)));
+    }
+    return Math.max(...arr);
+}
