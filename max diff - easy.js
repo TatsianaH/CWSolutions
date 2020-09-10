807c2275ab973e50d5f5f5e94b224b6117a47e81
TatsianaH <ourflsun@gmail.com>
2020-09-10 19:11:35 -0400

    add solution

--- max diff - easy.js
+++ max diff - easy.js
@@ -1,1 +1,7 @@
-//
+// codewars.com/kata/588a3c3ef0fbc9c8e1000095/train/javascript
+
+function maxDiff(list) {
+    if(list.length === 0) return 0;
+    list.sort((a, b) => a - b, 0);
+    return list[list.length - 1] - list[0];
+};
