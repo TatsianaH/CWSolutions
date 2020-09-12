1cf387546a034eae064dadb6e4fdd7dbb2a925cc
TatsianaH <ourflsun@gmail.com>
2020-09-12 13:43:46 -0400

    add solution

--- Divide and Conquer.js
+++ Divide and Conquer.js
@@ -1,1 +1,7 @@
-//
+// https://www.codewars.com/kata/57eaec5608fed543d6000021/train/javascript
+
+function divCon(x){
+    const num = x.filter(el => typeof el === 'number').reduce((acc, curr) => acc + curr, 0);
+    const str = x.filter(el => typeof el === 'string').reduce((acc, curr) => acc + +curr, 0);
+    return num - str;
+}
