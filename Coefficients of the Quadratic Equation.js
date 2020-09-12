16c9d989ebd0c04fa51d7eeafa3fc650eceae67f
TatsianaH <ourflsun@gmail.com>
2020-09-12 15:17:32 -0400

    add solution

--- Coefficients of the Quadratic Equation.js
+++ Coefficients of the Quadratic Equation.js
@@ -1,1 +1,9 @@
 // https://www.codewars.com/kata/5d59576768ba810001f1f8d6/train/javascript
+
+function quadratic(x1, x2){
+    // х² + px + q = 0
+    // ax^2 + bx + c = 0
+    const b = -x1 - x2;
+    const c = x1 * x2;
+    return [1, b, c];
+}
