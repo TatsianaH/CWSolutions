7dfa5cc75c2356640fa1bf065bfb48ac53d794f1
TatsianaH <ourflsun@gmail.com>
2020-09-12 17:46:15 -0400

    add solution

--- Show multiples of 2 numbers within a range.js
+++ Show multiples of 2 numbers within a range.js
@@ -1,1 +1,10 @@
 // https://www.codewars.com/kata/583989556754d6f4c700018e/train/javascript
+
+function multiples(s1,s2,s3){
+    const min = Math.min(s1, s2);
+    let arr = [];
+    for(let i = min; i < s3; i += min){
+        if(i % s1 === 0 && i % s2 === 0) arr.push(i);
+    }
+    return arr;
+}
