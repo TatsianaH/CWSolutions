e3b9274095259b3ad875b0d10e417e615c7c1843
TatsianaH <ourflsun@gmail.com>
2020-09-12 15:25:48 -0400

    add solution

--- Find the first non-consecutive number.js
+++ Find the first non-consecutive number.js
@@ -1,2 +1,8 @@
 // https://www.codewars.com/kata/58f8a3a27a5c28d92e000144/train/javascript
 
+function firstNonConsecutive (arr) {
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] - 1 !== arr[i - 1]) return arr[i];
+    }
+    return null;
+}
