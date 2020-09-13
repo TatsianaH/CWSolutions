8cf9e43d793bbb4b43f2cfbbdf028720386cc549
TatsianaH <ourflsun@gmail.com>
2020-09-13 16:40:28 -0400

    add solution

--- Find the sum of the roots of a quadratic equation.js
+++ Find the sum of the roots of a quadratic equation.js
@@ -1,1 +1,8 @@
 // https://www.codewars.com/kata/57d448c6ba30875437000138/train/javascript
+
+function roots(a,b,c){
+// discriminant D = b ** 2 − 4ac.
+    const d = b ** 2 - 4 * a * c;
+    if(d < 0) return null;
+    else return Math.round(-b / a * 100) / 100;
+}
