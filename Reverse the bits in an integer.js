b71d4284d16e44d636c21e6de2ae5004e332311b
TatsianaH <ourflsun@gmail.com>
2020-09-13 16:56:20 -0400

    add solution

--- Reverse the bits in an integer.js
+++ Reverse the bits in an integer.js
@@ -1,1 +1,8 @@
 // https://www.codewars.com/kata/5959ec605595565f5c00002b/train/javascript
+
+function reverseBits (n) {
+    n = (n).toString(2); // get in binary
+    n = n.split('').reverse().join(''); // reverse binary
+    n = parseInt(n, 2); // convert to decimal
+    return n;
+}
