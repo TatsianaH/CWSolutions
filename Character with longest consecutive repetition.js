42318df2fc04647f82e072c8585d96a09eb0f5c1
TatsianaH <ourflsun@gmail.com>
2020-09-15 18:42:02 -0400

    add solution

--- Character with longest consecutive repetition.js
+++ Character with longest consecutive repetition.js
@@ -1,1 +1,18 @@
 // https://www.codewars.com/kata/586d6cefbcc21eed7a001155/train/javascript
+
+function longestRepetition(s) {
+    if (s.length === 0) return ["", 0];
+    let arr = [];
+    let letter = s[0];
+    for(let i = 1; i <= s.length; i++){
+        if(s[i] === letter[letter.length - 1]) {
+            letter += s[i];
+        } else {
+            arr.push([letter[0], letter.length]);
+            letter = s[i];
+        }
+    }
+    const lengthOfRep = arr.map((el, i) => el[1]);
+    const max = Math.max(...lengthOfRep);
+    return arr.filter((el, i) => el[1] === max)[0];
+}
