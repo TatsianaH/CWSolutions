b5c27d0cdba34dcd2563c543ee9d252a513286b4
TatsianaH <ourflsun@gmail.com>
2020-09-17 18:53:57 -0400

    add solution

--- String array duplicates.js
+++ String array duplicates.js
@@ -1,1 +1,16 @@
-//
+// https://www.codewars.com/kata/59f08f89a5e129c543000069/train/javascript
+
+function dup(s) {
+    const arr = [];
+    for(let i = 0; i < s.length; i++){
+        let word = s[i][0];
+        for(let j = 1; j < s[i].length; j++){
+            if(s[i][j] !== word[word.length - 1]){
+                word += s[i][j];
+            }
+            continue;
+        }
+        arr.push(word);
+    }
+    return arr;
+};
