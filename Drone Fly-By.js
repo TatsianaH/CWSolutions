3ae60fa7bac553199c59201020cd8dcfccb32330
TatsianaH <ourflsun@gmail.com>
2020-09-17 17:04:40 -0400

    add solution

--- Drone Fly-By.js
+++ Drone Fly-By.js
@@ -1,1 +1,8 @@
 // https://www.codewars.com/kata/58356a94f8358058f30004b5/train/javascript
+
+function flyBy(lamps, drone){
+    const lampsLength = lamps.length;
+    const droneLength = drone.length;
+    const lampsOff = lampsLength - droneLength;
+    return droneLength >= lampsLength ? 'o'.repeat(lampsLength) :'o'.repeat(droneLength) + 'x'.repeat(lampsOff);
+}
