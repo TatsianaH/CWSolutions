f48567bbca8127e6bc0502b1e10ee936c5913567
TatsianaH <ourflsun@gmail.com>
2020-09-18 18:57:17 -0400

    add solution

--- Fun with lists: length.js
+++ Fun with lists: length.js
@@ -1,1 +1,10 @@
 // https://www.codewars.com/kata/581e476d5f59408553000a4b/train/javascript
+
+function length(head) {
+    let count = 0;
+    while(head) {
+        head = head.next;
+        count++;
+    }
+    return count;
+}
