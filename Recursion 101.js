ee15ac1fdeee6cb2f2eba0370f689ba8edf3b050
TatsianaH <ourflsun@gmail.com>
2020-09-18 19:35:38 -0400

    add solution

--- Recursion 101.js
+++ Recursion 101.js
@@ -3,16 +3,12 @@
 function solve(a,b){
     if(a === 0 || b === 0) return [a, b];
     if(a >= 2 * b){
-        while (a !== 0 || b !== 0){
-            a = a - 2 * b;
-            return solve(a, b)
-        }
+        a = a - 2 * b;
+        return solve(a, b);
     }
     if (b >= 2 * a){
-        while (a !== 0 || b !== 0){
-            b = b - 2 * a;
-            return solve(a, b);
-        }
+        b = b - 2 * a;
+        return solve(a, b);
     }
     return [a, b];
 }
