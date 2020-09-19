2b1ff845ef89c068ade9e714b2567444f92ba5f7
TatsianaH <ourflsun@gmail.com>
2020-09-19 18:52:46 -0400

    add solution

--- Holy cats.js
+++ Holy cats.js
@@ -1,1 +1,6 @@
 // https://www.codewars.com/kata/58ebfa6ef7cda1a3d4000048/train/javascript
+
+function holycats(input){
+    const arr = input.filter(el => el !== 'wicked' && el !== 'normal');
+    return arr.length === 0 ? [] : arr;
+}
