5535510f75c9e98d9eec381d68961709a0e33efd
TatsianaH <ourflsun@gmail.com>
2020-09-19 18:59:05 -0400

    add solution

--- Training JS #33: methods of Math---max() min() and abs().js
+++ Training JS #33: methods of Math---max() min() and abs().js
@@ -1,1 +1,11 @@
 // https://www.codewars.com/kata/5733d6c2d780e20173000baa/train/javascript
+
+function maxMin(arr1,arr2){
+    let arr = [];
+    for(let i = 0; i < arr1.length; i++){
+        arr.push(Math.abs(arr1[i] - arr2[i]));
+    }
+    const max = Math.max(...arr);
+    const min = Math.min(...arr);
+    return [max, min];
+}
