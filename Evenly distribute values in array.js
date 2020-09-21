55c63a3fad1e383c973d33643eb1c86604521dc5
TatsianaH <ourflsun@gmail.com>
2020-09-21 11:10:38 -0400

    add solution

--- Evenly distribute values in array.js
+++ Evenly distribute values in array.js
@@ -12,3 +12,19 @@ const distributeEvenly = (array) => {
     });
     return array;
 };
+
+// 2 case
+
+const distributeEvenly = (array) => {
+    let obj = {};
+    for(let s of array){
+        if(obj[s]) obj[s] += 1;
+        else obj[s] = 1;
+    }
+    const map = Object.entries(obj);
+    array = array.map((_, i) => {
+        if (map[i][1] > 1) map.push([map[i][0], map[i][1]- 1]);
+        return map[i][0];
+    });
+    return array;
+};
