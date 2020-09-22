6b16377fe18a77402bc4472d23994feb11234c25
TatsianaH <ourflsun@gmail.com>
2020-09-22 17:56:11 -0400

    add solution

--- Filling an array (part 2).js
+++ Filling an array (part 2).js
@@ -30,7 +30,7 @@ const random = (n, min, max) => {
 };
 
 const isPrime = num => {
-    for (let i = 2; i < num; i++) {
+    for (let i = 2; i <= Math.sqrt(num); i++) {
         if (num % i === 0) return false;
     }
     return true;
