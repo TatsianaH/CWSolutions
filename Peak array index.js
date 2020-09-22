8210e07edc7406699ac242d8290e73f797253fba
TatsianaH <ourflsun@gmail.com>
2020-09-22 17:14:09 -0400

    add solution

--- Peak array index.js
+++ Peak array index.js
@@ -1,1 +1,10 @@
 // https://www.codewars.com/kata/5a61a846cadebf9738000076/train/javascript
+
+function peak(arr){
+    for(let i = 1; i < arr.length; i++){
+        const part1 = arr.slice(0, i).reduce((a, b) => a + b, 0);
+        const part2 = arr.slice(i + 1).reduce((a, b) => a + b, 0);
+        if(part1 === part2) return i;
+    }
+    return -1;
+}
