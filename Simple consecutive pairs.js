750ff6b21f2bdfc689c4860754a9e48e45b2606f
TatsianaH <ourflsun@gmail.com>
2020-09-23 12:43:57 -0400

    add solution

--- Simple consecutive pairs.js
+++ Simple consecutive pairs.js
@@ -1,1 +1,9 @@
 // https://www.codewars.com/kata/5a3e1319b6486ac96f000049/train/javascript
+
+function pairs(arr){
+    let count = 0;
+    for(let i = 0; i < arr.length - 1; i += 2){
+        if(Math.abs(arr[i] - arr[i + 1]) === 1) count++;
+    }
+    return count;
+};
