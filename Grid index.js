926e2edf996c72d1ac691181981d6a5ebb52b0a8
TatsianaH <ourflsun@gmail.com>
2020-09-25 18:08:33 -0400

    add solution

--- Grid index.js
+++ Grid index.js
@@ -1,1 +1,10 @@
 // https://www.codewars.com/kata/5f5802bf4c2cc4001a6f859e/train/javascript
+
+function gridIndex(grid, indices) {
+    grid = grid.flat();
+    let word = '';
+    for(let i = 0; i < indices.length; i++){
+        if(grid[indices[i] - 1] !== undefined) word += grid[indices[i] - 1];
+    }
+    return word;
+}
