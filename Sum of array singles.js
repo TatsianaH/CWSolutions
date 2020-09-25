5bf418d9bc1d85786ae804df057b374b93c74b7f
TatsianaH <ourflsun@gmail.com>
2020-09-24 20:53:34 -0400

    add solution

--- Sum of array singles.js
+++ Sum of array singles.js
@@ -1,1 +1,6 @@
 // https://www.codewars.com/kata/59f11118a5e129e591000134/train/javascript
+
+function repeats(arr){
+    const arr1 = arr.filter(el => arr.indexOf(el) === arr.lastIndexOf(el));
+    return arr1[0] + arr1[1];
+};
