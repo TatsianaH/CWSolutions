606b0759e10c236adabc6011eed6c1e9e9d3daa1
TatsianaH <ourflsun@gmail.com>
2020-09-27 19:25:45 -0400

    add solution

--- String doubles.js
+++ String doubles.js
@@ -1,1 +1,10 @@
 // https://www.codewars.com/kata/5a145ab08ba9148dd6000094/train/javascript
+
+function doubles(s) {
+    let res = '';
+    for (const l of s) {
+        if (res.length != 0 && res[res.length - 1] == l) res = res.slice(0, -1);
+        else res += l;
+    }
+    return res;
+}
