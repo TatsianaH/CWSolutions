1e6d00bf6b976ba1f65f15185350c2a74ee5546c
TatsianaH <ourflsun@gmail.com>
2020-09-28 20:23:30 -0400

    add solution

--- Transform To Prime.js
+++ Transform To Prime.js
@@ -1,2 +1,18 @@
 // https://www.codewars.com/kata/transform-to-prime/train/javascript
 
+function minimumNumber(numbers){
+    const arrPrimes = [1];
+    nextPrime:
+        for(let i = 2; i <= 41000; i++){
+            for(let j = 2; j < Math.sqrt(i); j++){
+                if(i % j === 0) continue nextPrime;
+            }
+            arrPrimes.push(i);
+        }
+    let sum = numbers.reduce((a, b) => a + b, 0);
+    arrPrimes.push(sum);
+    arrPrimes.sort((a, b) => a - b);
+    let prime = arrPrimes.indexOf(sum) + 1;
+    let res = arrPrimes[prime] - sum;
+    return res;
+}
