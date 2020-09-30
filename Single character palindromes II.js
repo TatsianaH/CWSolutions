2515c488aef8277d1f914cac5d2ea65c4c8a39fe
TatsianaH <ourflsun@gmail.com>
2020-09-29 20:52:09 -0400

    add solution

--- Single character palindromes II.js
+++ Single character palindromes II.js
@@ -1,1 +1,14 @@
 // https://www.codewars.com/kata/5a66ea69e6be38219f000110/train/javascript
+
+function solve(str){
+    let count = 0;
+    let i = 0;
+    while(i < str.length / 2){
+        if(str[i] !== str[str.length - 1 - i]) {
+            count++;
+        }
+        i++;
+    }
+    if(count === 0 && str.length % 2 === 1) return true;
+    return count === 1 ? true : false;
+}
