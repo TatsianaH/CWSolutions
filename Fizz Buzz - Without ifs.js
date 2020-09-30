6330b369ea5018f248600d511c0f200acedb4d26
TatsianaH <ourflsun@gmail.com>
2020-09-30 19:51:08 -0400

    add solution

--- Fizz Buzz - Without ifs.js
+++ Fizz Buzz - Without ifs.js
@@ -1,1 +1,8 @@
 // https://www.codewars.com/kata/584c702256c98fc0a0000043/train/javascript
+
+const fizzBuzz = n => {
+    const result = n % 3 === 0 && n % 5 !== 0 ? 'Fizz' :
+        n % 3 !== 0 && n % 5 === 0 ? 'Buzz' :
+            n % 3 === 0 && n % 5 === 0 ? 'FizzBuzz' : n;
+    return result;
+};
