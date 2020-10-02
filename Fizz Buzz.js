95ecc804a2c75ce1854f5139a7409ad30cbd4ee6
TatsianaH <ourflsun@gmail.com>
2020-10-01 20:04:44 -0400

    add solution

--- Fizz Buzz.js
+++ Fizz Buzz.js
@@ -1,1 +1,12 @@
 // https://www.codewars.com/kata/5300901726d12b80e8000498/train/javascript
+
+function fizzbuzz(n){
+    let arr = [];
+    for(let i = 1; i <= n; i++){
+        if(i % 15 === 0) arr.push('FizzBuzz');
+        else if(i % 5 === 0) arr.push('Buzz');
+        else if(i % 3 === 0) arr.push('Fizz');
+        else arr.push(i);
+    }
+    return arr;
+}
