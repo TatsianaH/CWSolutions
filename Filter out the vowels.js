fee8ea862ef6b8aeba886a3ae3186e46f97c7936
TatsianaH <ourflsun@gmail.com>
2020-10-03 16:05:19 -0400

    add solution

--- Filter out the vowels.js
+++ Filter out the vowels.js
@@ -1,1 +1,6 @@
 // https://www.codewars.com/kata/5944367162ed20be2a00005d/train/javascript
+
+function vowelFilter (letters) {
+    const vowels = ["a", "e", "i", "o", "u"];
+    return letters.filter(letter => !vowels.includes(letter));
+};
