b28544610e36f7eb800dcd852e19d170d0143cbd
TatsianaH <ourflsun@gmail.com>
2020-10-05 19:21:38 -0400

    add solution

--- Fizz Buzz Cuckoo Clock.js
+++ Fizz Buzz Cuckoo Clock.js
@@ -5,7 +5,7 @@ function fizzBuzzCuckooClock(time) {
     if(time[1] === '00'){
         if(time[0] > 12) time[0] = time[0] - 12;
         if(time[0] === '00') time[0] = '12';
-        return 'Cuckoo '.repeat(+time[0]).trimRight();
+        return ('Cuckoo ').repeat(+time[0]).trimRight();
     }
     else if(time[1] % 30 === 0) return 'Cuckoo';
     else if(time[1] % 15 === 0) return 'Fizz Buzz';
