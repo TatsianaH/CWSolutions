20142070f513f20e8596cd48f4b7518995f55b60
TatsianaH <ourflsun@gmail.com>
2020-10-06 22:09:00 -0400

    add solution

--- Cat years, Dog years.js
+++ Cat years, Dog years.js
@@ -17,3 +17,11 @@ const humanYearsCatYearsDogYears = function(hY) {
     years[0] = hY;
     return years;
 }
+
+// 2 case
+
+const humanYearsCatYearsDogYears = function(y) {
+    if (y == 1) return [1, 15, 15];
+    if (y == 2) return [2, 24, 24];
+    return [y, (y-2) * 4 + 24, (y-2) * 5 + 24];
+}
