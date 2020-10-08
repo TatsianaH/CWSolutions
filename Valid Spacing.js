73bbde3b31c5be7e50541f281dff5ee63dba7c7d
TatsianaH <ourflsun@gmail.com>
2020-10-07 21:30:01 -0400

    add solution

---
+++ Valid Spacing.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5f77d62851f6bc0033616bd8/train/javascript
+
+function validSpacing(s) {
+    if(s.length < 1) return true;
+    if(s[0] === ' ') return false;
+    if(s[s.length - 1] === ' ') return false;
+    else {
+        for(let i = 1; i <= s.length; i++){
+            if(s[i] === ' ' && s[i - 1] === ' ') return false;
+        }
+    }
+    return true;
+}
