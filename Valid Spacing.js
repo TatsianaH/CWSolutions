be640fabe0910f08391a48edad75e5d1707ad2cd
TatsianaH <ourflsun@gmail.com>
2020-10-07 21:31:09 -0400

    add solution

--- Valid Spacing.js
+++ Valid Spacing.js
@@ -4,10 +4,8 @@ function validSpacing(s) {
     if(s.length < 1) return true;
     if(s[0] === ' ') return false;
     if(s[s.length - 1] === ' ') return false;
-    else {
-        for(let i = 1; i <= s.length; i++){
-            if(s[i] === ' ' && s[i - 1] === ' ') return false;
-        }
+    for(let i = 1; i <= s.length; i++){
+        if(s[i] === ' ' && s[i - 1] === ' ') return false;
     }
     return true;
 }
