582d7fef5370b6d86c4106e55b9b9ff17c45a5d2
TatsianaH <ourflsun@gmail.com>
2020-10-07 21:23:26 -0400

    add solution

--- Mirror, mirror, on the wall....js
+++ Mirror, mirror, on the wall....js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/5f55ecd770692e001484af7d/train/javascript
 
 function mirror(data) {
-    let asc = [...data].sort((a, b) => a - b);
-    let desc = [...data].sort((a, b) => b - a).slice(1);
+    const asc = [...data].sort((a, b) => a - b);
+    const desc = [...data].sort((a, b) => b - a).slice(1);
     return asc.concat(desc);
 }
