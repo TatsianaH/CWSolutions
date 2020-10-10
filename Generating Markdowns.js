55d9d541faba95e12069fd263c2e66e2a3198985
TatsianaH <ourflsun@gmail.com>
2020-10-09 23:03:36 -0400

    add solution

---
+++ Generating Markdowns.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5f656199132bf60027275739/train/javascript
+
+function generateMarkdowns(markdown,text,urlOrLanguage) {
+    if(markdown === 'link') return '[' + text + '](' + urlOrLanguage + ')';
+    if(markdown === 'img') return '![' + text + '](' + urlOrLanguage + ')';
+    else return '```' + urlOrLanguage + '\n' + text + '\n```';
+};
