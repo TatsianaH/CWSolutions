6eee37c76653e51497b1e37eddaccbbc2eb2a0cf
TatsianaH <ourflsun@gmail.com>
2020-10-12 19:53:47 -0400

    upd

--- Simple string reversal.js
+++ Simple string reversal.js
@@ -4,3 +4,14 @@ function solve(str){
     const arr = [...str.split(' ').join('')];
     return str.replace(/\S/g, a => arr.pop());
 }
+
+// 2 case
+
+function solve(str){
+    let s = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        if (str[i] !== ' ') s += str[i];
+        if (str[s.length] === ' ') s += str[s.length];
+    }
+    return s;
+}
