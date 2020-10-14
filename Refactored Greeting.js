344d525642f9e9ff8c43f2878e24f875149f319a
TatsianaH <ourflsun@gmail.com>
2020-10-14 19:45:11 -0400

    add solution

--- Refactored Greeting.js
+++ Refactored Greeting.js
@@ -1,1 +1,11 @@
 // https://www.codewars.com/kata/5121303128ef4b495f000001/train/javascript
+
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+
+    greet(yourName) {
+        return `Hello ${yourName}, my name is ${this.name}`;
+    }
+}
