17e2db621bbd7ea3349a1710867c60d727484adf
TatsianaH <ourflsun@gmail.com>
2020-10-21 13:40:31 -0400

    add solution

--- Larger Product or Sum.js
+++ Larger Product or Sum.js
@@ -1,1 +1,8 @@
 // https://www.codewars.com/kata/5c4cb8fc3cf185147a5bdd02/train/javascript
+
+function sumOrProduct(arr, n) {
+    arr.sort((a, b) => a - b);
+    let sum = arr.slice(-n).reduce((a,b) => a + b, 0);
+    let product = arr.slice(0, n).reduce((a, b) => a * b, 1);
+    return sum > product ? 'sum' : sum < product ? 'product' : 'same';
+}
