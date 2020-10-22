d67b731586e7ae711e8c1731b7b1e8bcefa1bb88
TatsianaH <ourflsun@gmail.com>
2020-10-22 19:28:53 -0400

    add solution

--- L2: Triple X.js
+++ L2: Triple X.js
@@ -1,1 +1,11 @@
 // https://www.codewars.com/kata/568dc69683322417eb00002c/train/javascript
+
+function tripleX(str){
+    if(str.includes('xxx')){
+        for(let i = 0; i < str.length; i++){
+            if(str.indexOf('x') === i) return str[i] + str[i + 1] + str[i + 2] === 'xxx';
+        }
+    }
+    return false;
+}
+
