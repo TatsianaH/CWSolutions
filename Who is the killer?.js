50fcc845ebce2827904e7da574c96b77ccfdac53
TatsianaH <ourflsun@gmail.com>
2020-10-24 18:41:58 -0400

    upd

---
+++ Who is the killer?.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5f709c8fb0d88300292a7a9d/train/javascript
+
+function killer(obj, arr) {
+    for(let key in obj){
+        let name = 0;
+        for(let i = 0; i < arr.length; i++){
+            if(obj[key].includes(arr[i])) name++;
+        }
+        if(name === arr.length) return key;
+    }
+}
