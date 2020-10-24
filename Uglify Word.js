cfcf95a354101d692c2c94bcc17418641246ff1d
TatsianaH <ourflsun@gmail.com>
2020-10-24 18:26:29 -0400

    upd

--- Uglify Word.js
+++ Uglify Word.js
@@ -1,1 +1,14 @@
 // https://www.codewars.com/kata/5ce6cf94cb83dc0020da1929/train/javascript
+
+function uglifyWord(s) {
+    let ans = '';
+    let flag = true;
+    for (let char of s){
+        if(flag) ans += char.toUpperCase();
+        else ans += char.toLowerCase();
+
+        if (char.toLowerCase() >= 'a' && char.toLowerCase() <= 'z') flag = !flag;
+        else flag = true;
+    }
+    return ans;
+}
