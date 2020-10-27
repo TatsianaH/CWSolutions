d9027faeb92524f72ea57b4c4e34f51f45c05f27
TatsianaH <ourflsun@gmail.com>
2020-10-26 21:33:41 -0400

    upd

---
+++ Consecutive items.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5f6d533e1475f30001e47514/train/javascript
+
+function consecutive(arr, a, b) {
+    const indA = arr.indexOf(a);
+    const indB = arr.indexOf(b);
+    if(indB - 1 === indA || indB + 1 === indA) return true;
+    else return false;
+}
