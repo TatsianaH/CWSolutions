61eb29ad7cef98eb47b15d26c7a94941bd1f4c00
TatsianaH <ourflsun@gmail.com>
2020-10-26 21:34:25 -0400

    upd

--- Consecutive items.js
+++ Consecutive items.js
@@ -6,3 +6,9 @@ function consecutive(arr, a, b) {
     if(indB - 1 === indA || indB + 1 === indA) return true;
     else return false;
 }
+
+// 2 variant
+
+function consecutive(arr, a, b) {
+    return Math.abs(arr.indexOf(a) - arr.indexOf(b)) == 1;
+}
