472693b90d37f05f0b7bb8042469e861d3b4386a
TatsianaH <ourflsun@gmail.com>
2020-11-02 17:32:14 -0500

    upd

--- Move 10.js
+++ Move 10.js
@@ -1,1 +1,14 @@
 // https://www.codewars.com/kata/57cf50a7eca2603de0000090/train/javascript
+
+function moveTen(s){
+    const alph = ' abcdefghijklmnopqrstuvwxyz';
+    let str = '';
+    for(let i = 0; i < s.length; i++){
+        let ind = alph.indexOf(s[i]) + 10;
+        if(ind > 26){
+            ind -= 26;
+        }
+        str += alph[ind];
+    }
+    return str;
+}
