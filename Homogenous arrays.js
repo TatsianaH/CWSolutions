3961d5c81f011e830a72b3ad198070b8b48c05e5
TatsianaH <ourflsun@gmail.com>
2020-11-04 19:01:58 -0500

    upd

--- Homogenous arrays.js
+++ Homogenous arrays.js
@@ -1,1 +1,21 @@
 // https://www.codewars.com/kata/57ef016a7b45ef647a00002d/train/javascript
+
+function filterHomogenous(a) {
+    let arr = [];
+    for(let i = 0; i < a.length; i++){
+        let temp = [];
+        for(let j = 0; j < a[i].length; j++){
+            const typeEl = typeof a[i][0];
+            if(typeof a[i][j] === typeEl) {
+                temp.push(a[i][j]);
+                if(j === a[i].length - 1){
+                    if(a[i].length === temp.length) {
+                        arr.push(temp);
+                        temp = [];
+                    }
+                }
+            }
+        }
+    }
+    return arr;
+}
