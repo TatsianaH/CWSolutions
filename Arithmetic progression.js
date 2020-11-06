27a39c961323397581ddf582bc5aaf5821c3f239
TatsianaH <ourflsun@gmail.com>
2020-11-05 19:40:30 -0500

    upd

--- Arithmetic progression.js
+++ Arithmetic progression.js
@@ -1,1 +1,11 @@
 // https://www.codewars.com/kata/55caf1fd8063ddfa8e000018/train/javascript
+
+function arithmeticSequenceElements(a,r,n) {
+    let seq = [];
+    let i = a;
+    while(seq.length < n){
+        seq.push(i);
+        i += r;
+    }
+    return seq.join(', ');
+}
