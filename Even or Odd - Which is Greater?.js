18377cfea2ca84c46be255ce3a5738ab88072fab
TatsianaH <ourflsun@gmail.com>
2020-11-07 20:32:26 -0500

    upd

--- Even or Odd - Which is Greater?.js
+++ Even or Odd - Which is Greater?.js
@@ -1,6 +1,5 @@
 // https://www.codewars.com/kata/57f7b8271e3d9283300000b4/train/javascript
 
-
 function evenOrOdd(str) {
     const odd = str.split('').map(el => +el).filter(num => num % 2 === 1).reduce((a, b) => a + b, 0);
     const even = str.split('').map(el => +el).filter(num => num % 2 === 0).reduce((a, b) => a + b, 0);
