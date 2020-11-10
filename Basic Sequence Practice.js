fc8b1cc8c335a130595febd2296f12021bcd680a
TatsianaH <ourflsun@gmail.com>
2020-11-10 14:53:24 -0500

    upd

--- Basic Sequence Practice.js
+++ Basic Sequence Practice.js
@@ -12,3 +12,15 @@ function sumOfN(n) {
     }
     return res;
 };
+
+// 2 variant
+
+function sumOfN(n) {
+    let array = [];
+    array[0] = 0;
+    for (let i = 1; i < Math.abs(n) + 1; i++) {
+        if (n > 0) array[i] = array[i - 1] + i;
+        else array[i] = array[i - 1] - i;
+    }
+    return array;
+};
