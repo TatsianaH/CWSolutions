c7f476a442757fafcd08d2a215ef5d48c67e854d
TatsianaH <ourflsun@gmail.com>
2020-11-13 23:47:02 -0500

    upd

--- Build a square.js
+++ Build a square.js
@@ -1,2 +1,10 @@
 // https://www.codewars.com/kata/59a96d71dbe3b06c0200009c/train/javascript
 
+function generateShape(int){
+    let sq = [];
+    for(let i = 0; i < int; i++){
+        sq[i] = ('+').repeat(int);
+        if(i !== int - 1) sq[i] += '\n';
+    }
+    return sq.join('');
+}
