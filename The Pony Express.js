c8b17da420ae5c17e3535877109c93694a32b54b
TatsianaH <ourflsun@gmail.com>
2020-12-02 18:49:13 -0500

    upd

--- The Pony Express.js
+++ The Pony Express.js
@@ -15,3 +15,17 @@ function riders(s) {
     }
     return count === 0 ? rider : count > 100 ? rider + 2 : rider + 1;
 }
+
+// 2 variant
+
+function riders(stations) {
+    let worker = 1, sum = 0;
+    for(let i = 0; i < stations.length; i++){
+        sum += stations[i];
+        if (sum > 100){
+            sum = stations[i];
+            worker += 1;
+        }
+    }
+    return worker;
+}
