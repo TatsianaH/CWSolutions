5acecca39ab91944b03dc6b966c7cce0c34fddc0
TatsianaH <ourflsun@gmail.com>
2020-12-04 00:00:58 -0500

    upd

--- Replace all items.js
+++ Replace all items.js
@@ -1,1 +1,19 @@
-//
+// https://www.codewars.com/kata/57ae18c6e298a7a6d5000c7a/train/javascript
+
+function replaceAll(seq, find, replace) {
+    let arr = [];
+    let str = '';
+    if(typeof seq === 'string'){
+        for(let l of seq){
+            if(l === find) str += replace;
+            else str += l;
+        }
+        return str;
+    } else {
+        for(let l of seq){
+            if(l === find) arr.push(replace);
+            else arr.push(l);
+        }
+        return arr;
+    }
+}
