a9c386be6eef0bb07192ee53e0db57c6ed2616d2
TatsianaH <ourflsun@gmail.com>
2020-12-05 18:45:40 -0500

    upd

--- Predict your age!.js
+++ Predict your age!.js
@@ -3,3 +3,7 @@
 function predictAge(...args){
     return Math.floor(Math.sqrt(args.map(el => el * el).reduce((a, b) => a + b, 0)) / 2);
 }
+
+// 2 variant
+
+const predictAge = (...ages) => Math.hypot(...ages) / 2 | 0;
