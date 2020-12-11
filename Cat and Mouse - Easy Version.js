2d51a1c4e49de194575cd9e6954ebe5b2d851573
TatsianaH <ourflsun@gmail.com>
2020-12-10 22:33:12 -0500

    upd

---
+++ Cat and Mouse - Easy Version.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/57ee24e17b45eff6d6000164/train/javascript
+
+function catMouse(x){
+    let count = 0;
+    for(let i = 0; i < x.length; i++){
+        if(x[i] === '.') count++;
+        if(x[i] === 'm' && count <= 3) return 'Caught!';
+    }
+    return 'Escaped!';
+}
