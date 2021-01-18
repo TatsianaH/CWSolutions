002d7604d16e0d0d176ac7d84a57351848d3ee4d
tanyahants <OURFLSUN@GMAIL.COM>
2021-01-17 23:42:38 -0500

    upd

---
+++ Ce*s*r*d Strings.js
@@ -0,0 +1,17 @@
+// https://www.codewars.com/kata/5ff6060ed14f4100106d8e6f/train/javascript
+
+
+
+
+function uncensor(n, d) {
+    let str = '';
+    for(let i = 0; i < n.length; i++){
+        if(n[i] === '*') {
+            str += d[0];
+            d = d.slice(1);
+        } else {
+            str += n[i];
+        }
+    }
+    return str;
+}
