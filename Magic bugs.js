e24a3a447211cb3cb7c4d937e7fcb39f3d8a818e
TatsianaH <ourflsun@gmail.com>
2021-01-22 21:40:59 -0500

    upd

---
+++ Magic bugs.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/56ccad525f907dc238000004/train/javascript
+
+function magic(input){
+    if (typeof input === 'undefined'){
+        return {bar: "hello"};
+    } else {
+        return {bar: "world"};
+    }
+}
