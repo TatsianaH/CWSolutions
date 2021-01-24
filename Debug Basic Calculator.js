36fa9270b85353d9cf7c8fca261585ceb702c9d2
TatsianaH <ourflsun@gmail.com>
2021-01-24 18:49:13 -0500

    upd

---
+++ Debug Basic Calculator.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/56368f37d464c0a43c00007f/train/javascript
+
+const calculate = function calculate(a, o, b) {
+    if(o === "+"){
+        return a + b;
+    } else if(o === "-"){
+        return a - b;
+    } else if(o === "/" && b !== 0){
+        return a / b;
+    } else if(o === "*"){
+        return a * b;
+    } else {
+        return null;
+    }
+}
