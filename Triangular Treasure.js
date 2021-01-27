d532a689a742f9d8cdf6bb84b85e14f937e7fba2
TatsianaH <ourflsun@gmail.com>
2021-01-27 11:28:31 -0500

    upd

--- Triangular Treasure.js
+++ Triangular Treasure.js
@@ -1,1 +1,9 @@
 // https://www.codewars.com/kata/525e5a1cb735154b320002c8/train/javascript
+
+function triangular(n) {
+    let arr = [];
+    for(let i = 1; i <= n; i++){
+        arr.push('*'.repeat(i));
+    }
+    return arr.join('').length;
+}
