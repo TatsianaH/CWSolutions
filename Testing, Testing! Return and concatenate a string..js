22afa51a7ba295304cb68eeee3d4903dcddf3a63
TatsianaH <ourflsun@gmail.com>
2021-01-26 21:11:26 -0500

    upd

--- Testing, Testing! Return and concatenate a string..js
+++ Testing, Testing! Return and concatenate a string..js
@@ -1,1 +1,6 @@
 // https://www.codewars.com/kata/5977387e131c07082b000098/train/javascript
+
+function getRes(myScore) {
+    const str1 = 'Thank you for taking part.';
+    return myScore >= 70 ? 'Congratulations, you have passed! ' + str1 : 'Sorry, you have failed. Better luck next time! ' + str1;
+}
