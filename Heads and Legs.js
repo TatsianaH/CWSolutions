b0c7ce0a4a57f3108d9348c13a1f2acada4eb30e
TatsianaH <ourflsun@gmail.com>
2021-02-10 22:32:10 -0500

    upd

---
+++ Heads and Legs.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/574c5075d27783851800169e/train/javascript
+
+function animals(heads, legs){
+    if(heads === 0 && legs === 0) return [0, 0];
+    let cows = (legs - heads * 2) / 2;
+    let chickens = heads - cows;
+    if(chickens >= 0 && Number.isInteger(chickens) && cows >= 0 && Number.isInteger(cows)){
+        return [chickens, cows];
+    }
+    return 'No solutions';
+}
+
+
