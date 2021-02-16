e4a6f429f16858464cbd298bedb014a48b4777ef
TatsianaH <ourflsun@gmail.com>
2021-02-15 23:26:51 -0500

    upd

---
+++ Multiply the number.js
@@ -0,0 +1,12 @@
+// codewars.com/kata/5708f682c69b48047b000e07/train/javascript
+
+function multiply(n){
+    let num = n.toString().split('');
+    let l = num.length;
+    if(n < 0){
+        l = l - 1;
+        num = num.slice(1);
+    }
+    num = num.join('') * (5 ** l);
+    return n < 0 ? (-1) * num : +num;
+}
