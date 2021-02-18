35ca453cf56cb32448b086db50d0a02877704be3
TatsianaH <ourflsun@gmail.com>
2021-02-17 22:07:15 -0500

    upd

--- Count cubes in a Menger Sponge.js
+++ Count cubes in a Menger Sponge.js
@@ -1,1 +1,9 @@
 // https://www.codewars.com/kata/59d28768a25c8c51a6000057/train/javascript
+
+function calc_ms(n) {
+    let cubes = 1;
+    for(let i = 1; i <= n; i++){
+        cubes = cubes * 20;
+    }
+    return cubes;
+}
