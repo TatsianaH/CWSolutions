e41c82399f177b45f2f5718991344740853740bd
TatsianaH <ourflsun@gmail.com>
2021-02-21 22:40:28 -0500

    upd

---
+++ Sum of values from 1 to n inclusive { nΣn=1 }.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/578a55517c77f535a8000064/train/javascript
+
+function total(num){
+    let sum = 0;
+    for(let i = 0; i <= num; i++){
+        sum += i;
+    }
+    return sum;
+}
