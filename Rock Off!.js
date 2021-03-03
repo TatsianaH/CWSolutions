038e83efe9807b378d524510167d74c360054fbe
TatsianaH <ourflsun@gmail.com>
2021-03-02 22:04:39 -0500

    upd

---
+++ Rock Off!.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/5b097da6c3323ac067000036/train/javascript
+
+function solve(a, b) {
+    let s1 = 0;
+    let s2 = 0;
+    if (a[0] > b[0]) s1++;
+    else if (a[0] < b[0]) s2++;
+    if (a[1] > b[1]) s1++;
+    else if (a[1] < b[1]) s2++;
+    if (a[2] > b[2]) s1++;
+    else if (a[2] < b[2]) s2++;
+    if (s1 > s2) return `${s1}, ${s2}: Alice made "Kurt" proud!`;
+    else if(s2 > s1) return `${s1}, ${s2}: Bob made "Jeff" proud!`;
+    else if (s1 === 1 && s2 === s1) return '1, 1: that looks like a "draw"! Rock on!';
+    else return '0, 0: that looks like a "draw"! Rock on!';
+}
