e263ef32560d80edc0aa3391d7cc742f6b70da0e
TatsianaH <ourflsun@gmail.com>
2021-03-03 22:48:25 -0500

    upd

---
+++ Series of integers from 0 to n.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5841f4fb673ea2a2ae000111/train/javascript
+
+function generateIntegers(n) {
+    let arr = [];
+    for(let i = 0; i <= n; i++){
+        arr.push(i);
+    }
+    return arr;
+}
+
