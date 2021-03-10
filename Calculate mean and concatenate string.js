7355f935ce8c3880d428039090f4513701b5d795
TatsianaH <ourflsun@gmail.com>
2021-03-09 19:31:58 -0500

    upd

--- Calculate mean and concatenate string.js
+++ Calculate mean and concatenate string.js
@@ -14,3 +14,17 @@ function mean(lst){
     const l = letter.reduce((a, b) => a + b, '');
     return [+n, l];
 }
+
+// 2 variant
+
+function mean(lst){
+    var num = 0, str = '';
+    lst.forEach(function(n) {
+        if(!isNaN(n)) {
+            num += +n;
+        }else {
+            str += n;
+        }
+    });
+    return [num / 10, str];
+}
