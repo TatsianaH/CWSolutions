21a38b470d4aa9138147065f2a73ec1f59886f75
TatsianaH <ourflsun@gmail.com>
2021-03-15 21:47:44 -0400

    upd

--- Training JS #16: Methods of String object--slice(), substring() and substr().js
+++ Training JS #16: Methods of String object--slice(), substring() and substr().js
@@ -1,1 +1,6 @@
 // https://www.codewars.com/kata/57274562c8dcebe77e001012/train/javascript
+
+function cutIt(arr){
+    let min = Math.min(...arr.map(el => el.length));
+    return arr.map(el => el.slice(0, min));
+}
