20778ef9bc03a68a289eaa3c283293528f253860
TatsianaH <ourflsun@gmail.com>
2021-03-20 23:36:35 -0400

    upd

---
+++ Training JS #21: Methods of String object--trim() and the string template.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/5729b103dd8bac11a900119e/train/javascript
+
+function fiveLine(s){
+    s = s.trim();
+    return `${s}\n${s}${s}\n${s}${s}${s}\n${s}${s}${s}${s}\n${s}${s}${s}${s}${s}`;
+}
