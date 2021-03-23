e0e9d0e4a2d15ed9fd22abe979104679c2cbce48
TatsianaH <ourflsun@gmail.com>
2021-03-22 20:51:32 -0400

    upd

--- Tally it up.js
+++ Tally it up.js
@@ -2,25 +2,12 @@
 
 const scoreToTally = function(score){
     let res = '';
-    while(score > 0){
-        if(score >= 5){
-            res += 'e <br>';
-            score -= 5;
-        } else if(score >= 4){
-            res += 'd';
-            score -= 4;
-        } else if(score >= 3){
-            res += 'c';
-            score -= 3;
-        } else if(score >= 2){
-            res += 'b';
-            score -= 2;
-        } else if(score >= 1){
-            res += 'a';
-            score -= 1;
-        }
+    const letters = ['', 'a','b', 'c', 'd', 'e'];
+    while(score >= 5){
+        res += 'e <br>';
+        score -= 5;
     }
-    return res;
+    return res + letters[score];
 }
 
 // 2 variant
