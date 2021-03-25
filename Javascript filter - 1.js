fe96058a44020ef7ced37c8f4dc55d72a11b3614
TatsianaH <ourflsun@gmail.com>
2021-03-25 00:03:35 -0400

    upd

--- Javascript filter - 1.js
+++ Javascript filter - 1.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/525d9b1a037b7a9da7000905/train/javascript
 
 function searchNames(l){
-    return l.filter((el, i) => el[0].endsWith('_'));
+    return l.filter(el => el[0].endsWith('_'));
 }
