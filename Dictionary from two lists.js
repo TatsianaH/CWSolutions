9479290ca597a71214596a7ef6cf7125a0de8a2f
TatsianaH <ourflsun@gmail.com>
2021-04-13 22:35:54 -0400

    upd

--- Dictionary from two lists.js
+++ Dictionary from two lists.js
@@ -9,3 +9,13 @@ function createDict(keys, values){
     }
     return obj;
 }
+
+// 2 variant
+
+function createDict(keys, values) {
+    const obj = {};
+    for (let i = 0; i < keys.length; i++) {
+        obj[keys[i]] = i < values.length ? values[i] : null;
+    }
+    return obj;
+}
