2f00396d728b43c9f8e3f3e3cc28d58d7408ade5
TatsianaH <ourflsun@gmail.com>
2021-04-15 22:47:06 -0400

    upd

---
+++ Coding Meetup #2 - Higher-Order Functions Series - Greet developers1.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/58279e13c983ca4a2a00002a/train/javascript
+
+function greetDevelopers(list) {
+    list.forEach(el => el.greeting = `Hi ${el.firstName}, what do you like the most about ${el.language}?`);
+    return list;
+}
